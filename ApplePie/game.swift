6245094dd7f5098f1d8a981fa978cb7972dfa209
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-11 15:49:26 +0300

    Create "Game" structure
    Defines current game's data

---
+++ ApplePie/game.swift
@@ -0,0 +1,15 @@
+//
+//  game.swift
+//  ApplePie
+//
+//  Created by Office-iMac on 2020-09-11.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import Foundation
+
+struct Game{
+    var currentWord:String
+    var remainingIncorrectAnswers: Int
+    
+}
